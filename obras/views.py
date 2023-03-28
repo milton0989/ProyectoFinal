01672ceda0e7d49ad5b4cc5b5392b2bceaf51729
Milton AVP <milton.suscripciones@gmail.com>
2023-03-28 10:57:32 -0300

    se agrego Autorizacion y Autenticacion

--- obras/views.py
+++ obras/views.py
@@ -4,31 +4,32 @@ from django.views.generic import ListView, DetailView, DeleteView, UpdateView, C
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 def index(request):
     return render(request, "obras/index.html")
 
-class ObraList(ListView):
+class ObraList(LoginRequiredMixin, ListView):
     model = Obra
     context_object_name = "obras"
 
-class ObraDetalle(DetailView):
+class ObraDetalle(LoginRequiredMixin, DetailView):
     model = Obra
     context_object_name = "obra"
 
-class ObraActualizar(UpdateView):
+class ObraActualizar(LoginRequiredMixin, UpdateView):
     model = Obra
     success_url = reverse_lazy("obra-list")
     fields = '__all__'
 
-class ObraEliminar(DeleteView):
+class ObraEliminar(LoginRequiredMixin, DeleteView):
     model = Obra
     context_object_name = "obra"
     success_url = reverse_lazy("obra-list")
 
-class ObraCrear(CreateView):
+class ObraCrear(LoginRequiredMixin, CreateView):
     model = Obra
     success_url = reverse_lazy("obra-list")
     fields = '__all__'
